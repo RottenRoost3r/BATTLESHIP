23e261a7380bfd22c51e25fb4b749d0176a40f25
Jordan Endicott <jordan.endicott823@gmail.com>
2018-12-05 10:54:09 -0500

    pass, ship exists

--- boats.rb
+++ boats.rb
@@ -1,3 +1,4 @@
 class Ship
-    
+    def initialize()
+    end
 end
--- tests/boats_test.rb
+++ tests/boats_test.rb
@@ -6,5 +6,10 @@ class Boats < Minitest::Test
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
-    
+
+    def test_that_ship_exists
+        x = Ship.new
+        assert_equal(Ship, x.class)
+    end
+
 end
