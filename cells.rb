c1f8587acc9900cdf63647fa31bc4ead1b963918
Jordan Endicott <jordan.endicott823@gmail.com>
2018-12-06 12:19:36 -0500

    working on cells

--- cells.rb
+++ cells.rb
@@ -2,7 +2,7 @@ require_relative "master.rb"
 
 class Cell
     def initialize(coordinates)
-        @contents = contents
+        @contents ||= "Water"
         @coordinates = coordinates
     end
     attr_reader :contents
