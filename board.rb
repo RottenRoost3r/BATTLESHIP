8a5cb83460786cdddd3a36e6f6c2b076ed940faa
Jordan Endicott <jordan.endicott823@gmail.com>
2018-12-12 12:13:46 -0500

    fixed grid to show rows and collums, working on making it more effiecient

--- board.rb
+++ board.rb
@@ -12,18 +12,29 @@ class Grid
     attr_reader :size
 end
 
-board = Grid.new(6)
+board = Grid.new(12)
 board.grid[1][1].occupy(Ship.new(4, "B"))
 board.grid[1][2].occupy(Ship.new(4, "B"))
 board.grid[1][3].occupy(Ship.new(4, "B"))
 board.grid[1][4].occupy(Ship.new(4, "B"))
+counter = 1
+letters = ('A'..'Z').to_a
+print "-"
+board.grid.each_with_index do |v, i|
+    
+    print "   #{letters[i]}"
+end
+puts "\n"
 
-board.grid.each do |v|
+board.grid.each_with_index do |v, i|
     v.each_with_index do |k, i|
-        if i == board.end_point
-            print "(#{k.to_s}) \n"
+        if i == 0
+            print " #{counter} (#{k.to_s})"
+            counter += 1
+        elsif i == board.end_point
+            print " (#{k.to_s}) \n"
         else
-            print "(#{k.to_s})"
+            print " (#{k.to_s})"
         end
     end
 end
