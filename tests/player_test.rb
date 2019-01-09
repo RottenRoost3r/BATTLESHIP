9e0d7e2b9a40293140d1f8db2b5e6d211d62ef92
Jordan Endicott <jordan.endicott823@gmail.com>
2019-01-09 10:37:20 -0500

    player class is a class

--- tests/player_test.rb
+++ tests/player_test.rb
@@ -6,4 +6,9 @@ class Players < Minitest::Test
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
+
+    def test_player_something
+        me = Player.new("jordan")
+        assert_equal(Player, me.class)
+    end
 end
