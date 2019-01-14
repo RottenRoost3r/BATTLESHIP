780922b9250f1a256d450bb4f0deabe4af9480f6
Jordan Endicott <jordan.endicott823@gmail.com>
2019-01-13 23:59:45 -0500

    enemy has coordinates to choose from

--- enemy.rb
+++ enemy.rb
@@ -6,11 +6,18 @@ class Enemy
         num = []
         @coordinates = []
         @ships = ["Carrier" => Ship.new(5, "C"), "Battleship" => Ship.new(4, "B"), "Destroyer" => Ship.new(3, "D"), "Patrol" => Ship.new(2, "P")]
+        counter = 0
         grid.size.times do
+            num << counter
+            counter += 1
         end
-            
-        
+        num.each do |row|
+            num.each do |col|
+                @coordinates << [row, col]
+            end 
+        end
     end
+
     attr_reader :grid
     attr_reader :ships
     attr_reader :coordinates
