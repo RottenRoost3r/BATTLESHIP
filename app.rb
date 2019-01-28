1fa0c800a1e162bee027ae950c26493fceee360e
Jordan Endicott <jordan.endicott823@gmail.com>
2019-01-28 10:24:03 -0500

    passing board size, creating board

--- app.rb
+++ app.rb
@@ -3,12 +3,18 @@ require_relative 'board.rb'
 require_relative 'boats.rb'
 require_relative 'cells.rb'
 require_relative 'enemy.rb'
+enable :sessions
 
 get '/' do
-  erb :open
+  session[:size] = params[:size]
+  size = session[:size]
+  erb :open, locals: {size: size}
 end
 
 post '/board' do
-  
+  session[:size] = params[:size]
+  board = Grid.new(session[:size].to_i)
+  params[:board] = board
+  erb :board
 end
 
