1a534b377f8f816597aafdb557e022e5b0e49c7d
Jordan Endicott <jordan.endicott823@gmail.com>
2019-02-03 19:26:59 -0500

    adding elements to gameplay page

--- app.rb
+++ app.rb
@@ -19,10 +19,18 @@ end
 get '/board' do
   board = session[:board]
   nme_board = session[:nme_board]
-  erb :board, locals: {board: board, nme_board: nme_board}
+  row = session[:row].to_i || 0
+  col = session[:col].to_i || 0
+  orientation = session[:orientation].to_s || ""
+  erb :board, locals: {board: board, nme_board: nme_board, row: row, col: col, orientation: orientation}
 end
 
 post '/board' do
+  session[:err] = ""
+  session[:row] = params[:row]
+  session[:col] = params[:col]
+  session[:orientation] = params[:orientation]
+  ships = [carrier = Ship.new(5, "(C)"), battleship = Ship.new(4, "(B)"), destroyer = Ship.new(3, "(D)"), patrol = Ship.new(2, "(P)")]
   redirect '/board'
 end
 
